1e727dedc9b1ec4d3c37b4390fb8295e9ef677e2
Ramin <eslamiramin85@gmail.com>
2023-05-09 16:55:29 +0330

    End

--- RaminEslami_HW3/Exercise4.py
+++ RaminEslami_HW3/Exercise4.py
@@ -49,21 +49,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#
-# birthday = datetime.strptime(birthday_str, "%Y-%m-%d")
-#
-# age_in_seconds = (datetime.now() - birthday).total_seconds()
-#
-# current_date = datetime.now()
-# next_birthday = datetime(current_date.year, birthday.month, birthday.day)
-#
-# if next_birthday < current_date:
-#     next_birthday = datetime(current_date.year + 1, birthday.month, birthday.day)
-#
-# time_to_birthday = next_birthday - current_date
-# days_to_birthday = time_to_birthday.days
-# minutes_of_birthday = int(time_to_birthday.total_seconds() // 60)
-# print(type(time_to_birthday))
-# shamsi_birthday = jdatetime.datetime.fromgregorian(datetime=birthday)
-# #
-# # print(type(age_in_seconds))
