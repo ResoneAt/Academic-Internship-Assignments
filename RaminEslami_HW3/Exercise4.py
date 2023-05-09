74422f1d80782c6eaa4feb05a1a1f2867ccd225b
Ramin <eslamiramin85@gmail.com>
2023-05-09 17:58:01 +0330

    end

--- RaminEslami_HW3/Exercise4.py
+++ RaminEslami_HW3/Exercise4.py
@@ -3,11 +3,22 @@ import jdatetime
 
 
 def seconds_of_life(birthday: datetime) -> float:
+    """
+    This function takes the date of birt day and returns the age in seconds
+    :param birthday: birthday in datetime format
+    :return: age in seconds
+    """
     age_in_seconds = (datetime.now() - birthday).total_seconds()
     return age_in_seconds
 
 
 def time_to_next_birthday(birthday: datetime) -> timedelta:
+    """
+    this function takes the date of birth and returns the amount of time left
+    to the birth in timedelta format
+    :param birthday: birthday in datetime format
+    :return: the amount of time left until birth in timedelta format
+    """
     now = datetime.now()
     next_birthday = datetime(now.year, birthday.month, birthday.day)
     if next_birthday < now:
@@ -16,13 +27,25 @@ def time_to_next_birthday(birthday: datetime) -> timedelta:
     return time_to_birthday
 
 
-def shamsi_birthday(birthday: datetime) -> datetime:
+def shamsi_birthday(birthday: datetime) -> str:
+    """
+    this function converts shamsi to AD
+    :param birthday: date of birth in AD
+    :return: date of birth in shamsi
+    """
     birthday = jdatetime.datetime.fromgregorian(datetime=birthday)
     shamsi = str(birthday.strftime("%Y-%m-%d"))
     return shamsi
 
 
-def happy_birthday(date_of_birth):
+def happy_birthday(date_of_birth: str) -> tuple | str:
+    """
+    This function receives the date of birth and displays the total number of seconds of his life in the output,
+    and also calculates the number of days and minutes remaining until the next birthday and congratulates him,
+    and also converts and displays the date of birth in shamsi .
+    :param date_of_birth: birthday in datetime format
+    :return: tuple of age in seconds , days to birthday, minutes of to birthday, congratulates message, shamsi birth
+    """
     try:
         birthday = datetime.strptime(date_of_birth, "%Y-%m-%d")
         seconds = seconds_of_life(birthday)
@@ -34,7 +57,7 @@ def happy_birthday(date_of_birth):
         return seconds, days_to_birthday, minutes_of_to_birthday, message, shamsi
 
     except ValueError:
-        return 'error'
+        return 'check your input, You have ValueError... try again'
 
 
 def main():
